4f2bbc7b371f3b6049a85984f2a3147f2b40af41
Lyneshia Correa <122991047+lyneshia@users.noreply.github.com>
2026-09-24 13:17:31 +0530

    Refactor rag.py to enhance document metadata handling
    Removed print statements and added source metadata to documents.

--- rag.py
+++ rag.py
@@ -47,14 +47,8 @@ def process_urls(urls):
             loader = UnstructuredLoader(web_url=u)
             loaded_docs = loader.load()
 
-            print(f"URL: {u}")
-            print(f"Documents loaded: {len(loaded_docs)}")
-
-            for doc in loaded_docs[:3]:
-                print(
-                    "Content length:",
-                    len(doc.page_content)
-                )
+            for doc in loaded_docs:
+                doc.metadata["source"] = u
 
             data.extend(loaded_docs)
 
@@ -69,9 +63,9 @@ def process_urls(urls):
     yield "Splitting the data into chunks..."
 
     text_splitter = RecursiveCharacterTextSplitter(
-    separators=["\n\n", "\n", ".", " "],
-    chunk_size=CHUNK_SIZE,
-    chunk_overlap=150
+        separators=["\n\n", "\n", ".", " "],
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=150
     )
 
     docs = text_splitter.split_documents(data)
@@ -83,8 +77,9 @@ def process_urls(urls):
         )
 
 
-    print("Total source documents:", len(data))
-    print("Total chunks:", len(docs))
+    for doc in docs:
+        if "source" not in doc.metadata:
+            doc.metadata["source"] = "Unknown"
 
 
     yield "Adding the chunks to the Vector database..."
@@ -98,11 +93,6 @@ def process_urls(urls):
         documents=docs,
         ids=uuids
     )
-
-    # Add 'source' metadata for RetrievalQAWithSourcesChain
-    for doc in docs:
-        if "source" not in doc.metadata and "url" in doc.metadata:
-            doc.metadata["source"] = doc.metadata["url"]
 
     yield "Adding the chunks to the Vector database..."
     uuids = [str(uuid4()) for _ in range(len(docs))]
